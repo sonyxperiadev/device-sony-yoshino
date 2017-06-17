83423c2f2cc99f4964fa6700eb31671ece0d348d
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-17 18:31:04 +0200

    fix wlan fw ln
    in the bringup there was a typo and firmware is not linked to the right location
    This patch solves the issue so that fw can be loaded properly
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -68,6 +68,6 @@ $(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /firmware rfs/msm/adsp/
 $(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /vendor/firmware rfs/msm/adsp/readonly/vendor && popd > /dev/null)
 
 $(shell mkdir -p $(PRODUCT_OUT)/system/etc/firmware/wlan/qca_cld)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini /etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /vendor/firmware/wlan/qca_cld/bdwlan.bin /etc/firmware/wlan/qca_cld/bdwlan.bin && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /data/misc/wifi/wlan_mac.bin /etc/firmware/wlan/qca_cld/wlan_mac.bin && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /vendor/firmware/wlan/qca_cld/bdwlan.bin etc/firmware/wlan/qca_cld/bdwlan.bin && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /data/misc/wifi/wlan_mac.bin etc/firmware/wlan/qca_cld/wlan_mac.bin && popd > /dev/null)
