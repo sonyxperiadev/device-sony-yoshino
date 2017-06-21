67015c12d495d37a757c72c7bbb5b507c39c2173
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-21 09:17:06 +0200

    add the FPC version selector
    this flag has to be set to select the FPC TZ firmware version
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -92,4 +92,7 @@ BOARD_SEPOLICY_DIRS += $(PLATFORM_COMMON_PATH)/sepolicy
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 TARGET_USES_HWC2 := true
 
+# FPC verstion select
+TARGET_FPC_VERSION := N
+
 include device/sony/common/CommonConfig.mk
