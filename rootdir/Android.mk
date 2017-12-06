fab49426665b9b34c6d59bc763da4bf3529f5813
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-12-06 06:54:36 +0100

    move recovery to common
    init.recovery has been moved to common as init.recovery.common.rc
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -30,16 +30,6 @@ LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.recovery.$(TARGET_DEVICE)
-LOCAL_SRC_FILES := init.recovery.yoshino.rc
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.recovery.$(TARGET_DEVICE)
-LOCAL_MODULE_SUFFIX := .rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := ueventd.$(TARGET_DEVICE)
 LOCAL_SRC_FILES := ueventd.yoshino.rc
 LOCAL_MODULE_TAGS := optional
