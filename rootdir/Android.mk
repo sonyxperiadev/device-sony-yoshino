6747295c0f09b12258d9930526054e32c12b781c
Alin Jerpelea <alin.jerpelea@sony.com>
2018-01-04 17:28:34 +0100

    ftfp: move simlinks to common
    all blob configuration will be moved to common for easy maintenance
    
    NOTE:
    this change requires at least Android 8.0  vendor v8
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,28 +38,3 @@ LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-$(shell mkdir -p $(PRODUCT_OUT)/system/rfs/msm/mpss/readonly/vendor)
-$(shell mkdir -p $(PRODUCT_OUT)/system/rfs/msm/adsp/readonly/vendor)
-$(shell mkdir -p $(PRODUCT_OUT)/system/rfs/msm/slpi/readonly/vendor)
-
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /data/tombstones/modem rfs/msm/mpss/ramdumps && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/rfs/msm/mpss rfs/msm/mpss/readwrite && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/rfs/shared rfs/msm/mpss/shared && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/hlos_rfs/shared rfs/msm/mpss/hlos && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /firmware rfs/msm/mpss/readonly/firmware && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /odm/firmware rfs/msm/mpss/readonly/vendor/firmware && popd > /dev/null)
-
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /data/tombstones/lpass rfs/msm/adsp/ramdumps && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/rfs/msm/adsp rfs/msm/adsp/readwrite && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/rfs/shared rfs/msm/adsp/shared && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/hlos_rfs/shared rfs/msm/adsp/hlos && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /firmware rfs/msm/adsp/readonly/firmware && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /odm/firmware rfs/msm/adsp/readonly/vendor/firmware && popd > /dev/null)
-
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /data/tombstones/lpass rfs/msm/slpi/ramdumps && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/rfs/msm/slpi rfs/msm/slpi/readwrite && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/rfs/shared rfs/msm/slpi/shared && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /persist/hlos_rfs/shared rfs/msm/slpi/hlos && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /firmware rfs/msm/slpi/readonly/firmware && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/system > /dev/null && ln -s /odm/firmware rfs/msm/slpi/readonly/vendor/firmware && popd > /dev/null)
