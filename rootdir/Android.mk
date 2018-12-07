5646b9dc3efc75e19735495a2ffeb3498dde1e7e
Alin Jerpelea <alin.jerpelea@sony.com>
2018-12-07 14:38:40 +0100

    fstab: add legacy fstab for devices without vendor partition
    on this platform we have 1 device that does not have vendor partition
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(TARGET_DEVICE), maple)
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := vendor/etc/fstab_legacy.yoshino
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := fstab.$(TARGET_DEVICE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
+include $(BUILD_PREBUILT)
+else
 include $(CLEAR_VARS)
 LOCAL_MODULE := fstab.$(TARGET_DEVICE)
 LOCAL_SRC_FILES := vendor/etc/fstab.yoshino
@@ -8,6 +18,7 @@ LOCAL_MODULE_STEM := fstab.$(TARGET_DEVICE)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
 include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.yoshino
